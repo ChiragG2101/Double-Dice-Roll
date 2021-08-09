33806a9dd0b46171c383582944cc9a4675c967ab
Chirag <cgupta7.rag2101@gmail.com>
2021-08-09 14:49:48 +0530

    complete

--- index.js
+++ index.js
@@ -19,16 +19,26 @@ switch (randomNumber1)
 var randomNumber2=Math.floor(Math.random()*6 +1);
 
 switch (randomNumber2)
- {case 1: document.querySelector("id="2"").src="images/dice1.png";
+ {case 1: document.getElementById("2").src="images/dice1.png";
             break;
- case 2: document.querySelector("id="2"").src="images/dice2.png";
+ case 2: document.getElementById("2").src="images/dice2.png";
             break;
- case 3: document.querySelector("id="2"").src="images/dice3.png";
+ case 3: document.getElementById("2").src="images/dice3.png";
             break;
- case 4: document.querySelector("id="2"").src="images/dice4.png";
+ case 4: document.getElementById("2").src="images/dice4.png";
             break;
- case 5: document.querySelector("id="2"").src="images/dice5.png";
+ case 5: document.getElementById("2").src="images/dice5.png";
             break;
- case 6: document.querySelector("id="2"").src="images/dice6.png";
+ case 6: document.getElementById("2").src="images/dice6.png";
             break;
  }
+
+ if (randomNumber1===randomNumber2) {
+     document.querySelector("h1").innerHTML="🚩Draw🚩";
+    }
+ else if(randomNumber2>randomNumber1) {
+     document.querySelector("h1").innerHTML="Player 2 Wins🚩";
+     }
+ else{
+     document.querySelector("h1").innerHTML="🚩Player 1 Wins ";
+     }
