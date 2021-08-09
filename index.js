f76c70b2409a477b39f2a783f5644f6576b6264d
Chirag <cgupta7.rag2101@gmail.com>
2021-08-09 13:27:58 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+var randomNumber1=Math.floor(Math.random()*6 +1);
+
+switch (randomNumber1)
+ {case 1: document.querySelector("img").src="images/dice1.png";
+            break;
+ case 2: document.querySelector("img").src="images/dice2.png";
+            break;
+ case 3: document.querySelector("img").src="images/dice3.png";
+            break;
+ case 4: document.querySelector("img").src="images/dice4.png";
+            break;
+ case 5: document.querySelector("img").src="images/dice5.png";
+            break;
+ case 6: document.querySelector("img").src="images/dice6.png";
+            break;
+ }
+var randomNumber2=Math.floor(Math.random()*6 +1);
+
+switch (randomNumber2)
+ {case 1: document.querySelector("id="2"").src="images/dice1.png";
+            break;
+ case 2: document.querySelector("id="2"").src="images/dice2.png";
+            break;
+ case 3: document.querySelector("id="2"").src="images/dice3.png";
+            break;
+ case 4: document.querySelector("id="2"").src="images/dice4.png";
+            break;
+ case 5: document.querySelector("id="2"").src="images/dice5.png";
+            break;
+ case 6: document.querySelector("id="2"").src="images/dice6.png";
+            break;
+ }
